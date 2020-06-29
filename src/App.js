b9e0a5e8a9e3d3b91a1361fe1388afe02ddd88d9
khardrix <carmenandryanhuffman@yahoo.com>
2020-06-29 18:54:19 -0400

    fixed warning about importing Component and using React.Component instead of Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
     constructor(){
         super()
         this.state = {
