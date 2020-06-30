d83a661bb662ab5c346c4f753e065da1cf67d622
khardrix <carmenandryanhuffman@yahoo.com>
2020-06-30 18:05:49 -0400

    meaningOfLife now starts at 48, now uses prevState and prevProps inside handleClick

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state = {
-            meaningOfLife: 47
+            meaningOfLife: 47 + this.props.increment
         }
     }
 
     handleClick = () => {
         this.setState((prevState, prevProps) => {
-            return {meaningOfLife: prevState.meaningOfLife + 1}
+            return {meaningOfLife: prevState.meaningOfLife + prevProps.increment}
         }, 
             () => console.log(this.state.meaningOfLife))
     }
